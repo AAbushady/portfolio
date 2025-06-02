e3f9a9daaa75dea98d0fd113f0a1b8146b4e57ff
Alexander Abushady <44341163+AAbushady@users.noreply.github.com>
2025-06-02 15:09:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://epiculous.sillytilly.org",
-    base: "/portfolio"
+    base: "/portfolio",
+    output: "static"
 });
